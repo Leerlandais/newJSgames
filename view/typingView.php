b96229f30c9dd64f07755ae565a1c6233d34b375
leerlandais <leerlandais@gmail.com>
2024-04-03 18:44:00 +0200

    restarted it, much better now

--- view/typingView.php
+++ view/typingView.php
@@ -7,25 +7,18 @@
     <title><?=$title?></title>
 </head>
 <body>
+    <div class="global">
     <h1>Games in Javascript</h1>
     <ul>
         <a href="?pg=home"><li>Home</li></a>
-        <a href=""><li></li></a>
-        <a href=""><li></li></a>
-        <a href=""><li></li></a>
-        <a href=""><li></li></a>
-        <a href=""><li></li></a>
     </ul>
     <h6 id="screenwidth"></h6>
 
-    <fieldset>
-        <legend>Type This As Fast As You Can</legend>
-        <p id="textToType">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis quis molestias nemo, libero facere repudiandae ut eaque fugit nostrum quod nisi sed recusandae sequi, dolores quo est temporibus ipsam commodi!</p>
+    <fieldset id="typingField">
     </fieldset>
-    <form action="">
-        <textarea name="" id="typeTextarea" cols="30" rows="10"></textarea>
-    </form>
-    <button id="startTyping">I wanna TYPE</button>
+    
+    <button id="startTypingGame">Press To Begin</button>
+    </div> <!--  end global -->
     <script src="scripts/typing.js"></script>
 </body>
 </html>
